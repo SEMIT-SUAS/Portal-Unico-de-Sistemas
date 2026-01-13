9778a503331aee27e28861a2f00e06084326f85d
fpss34 <fb2312vas@gmail.com>
2026-01-13 17:54:34 -0300

    'alter-v17'

--- Backend/src/config/database.ts
+++ Backend/src/config/database.ts
@@ -7,7 +7,7 @@ dotenv.config();
 // Configuração do pool de conexões
 const dbConfig: PoolConfig = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   database: process.env.DB_NAME,
   user: process.env.DB_USER ,
   password: process.env.DB_PASSWORD,
